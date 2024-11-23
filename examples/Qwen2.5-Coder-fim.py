3a9d549c63f1b54fe06705b9ed0ca949da1ca703
LuYF-Lemon-love <3555028709@qq.com>
2024-11-23 22:54:39 +0800

    000

--- examples/Qwen2.5-Coder-fim.py
+++ examples/Qwen2.5-Coder-fim.py
@@ -1,9 +1,9 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
-# load model
-device = "cuda" # the device to load the model onto
 
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Coder-0.5B")
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-Coder-0.5B", device_map="auto").eval()
+model_name = "Qwen/Qwen2.5-Coder-0.5B"
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
 
 input_text = """<|fim_prefix|>def quicksort(arr):
     if len(arr) <= 1:
@@ -14,12 +14,10 @@ input_text = """<|fim_prefix|>def quicksort(arr):
     right = [x for x in arr if x > pivot]
     return quicksort(left) + middle + quicksort(right)<|fim_middle|>"""
 
-model_inputs = tokenizer([input_text], return_tensors="pt").to(device)
+model_inputs = tokenizer([input_text], return_tensors="pt").to(model.device)
 eos_token_ids = [151659, 151660, 151661, 151662, 151663, 151664, 151645, 151643]
 
-# Use `max_new_tokens` to control the maximum output length.
 generated_ids = model.generate(model_inputs.input_ids, max_new_tokens=512, do_sample=False, eos_token_id=eos_token_ids)[0]
-# The generated_ids include prompt_ids, we only need to decode the tokens after prompt_ids.
 output_text = tokenizer.decode(generated_ids[len(model_inputs.input_ids[0]):], skip_special_tokens=True)
 
 print(f"Prompt: {input_text}\n\nGenerated text: {output_text}")
