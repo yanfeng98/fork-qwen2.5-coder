7069fd857c48445ea2c5f7f90fab491741647f1a
LuYF-Lemon-love <3555028709@qq.com>
2024-11-23 21:55:47 +0800

    000

--- examples/Qwen2.5-Coder-Instruct-stream.py
+++ examples/Qwen2.5-Coder-Instruct-stream.py
@@ -2,29 +2,36 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 from transformers import TextIteratorStreamer
 from threading import Thread
 
-device = "cuda" # the device to load the model onto
+model_name = "Qwen/Qwen2.5-Coder-0.5B-Instruct"
 
-# Now you do not need to add "trust_remote_code=True"
-tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Coder-32B-Instruct")
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-Coder-32B-Instruct", device_map="auto").eval()
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
 
-# Instead of using model.chat(), we directly use model.generate()
-# But you need to use tokenizer.apply_chat_template() to format your inputs as shown below
 prompt = "write a quick sort algorithm."
 messages = [
     {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
     {"role": "user", "content": prompt}
 ]
-text = tokenizer.apply_chat_template(
+input_ids = tokenizer.apply_chat_template(
     messages,
-    tokenize=False,
-    add_generation_prompt=True
-)
-model_inputs = tokenizer([text], return_tensors="pt").to(device)
+    add_generation_prompt=True,
+    return_tensors="pt"
+).to(model.device)
 
 streamer = TextIteratorStreamer(tokenizer, skip_prompt=True, skip_special_tokens=True)
 
-generation_kwargs = dict(inputs=model_inputs.input_ids, streamer=streamer, max_new_tokens=2048)
+generation_kwargs = dict(
+    inputs=input_ids,
+    streamer=streamer,
+    max_new_tokens=2048,
+    do_sample=True,
+    temperature=0.6,
+    top_p=0.9,
+)
 thread = Thread(target=model.generate, kwargs=generation_kwargs)
 
 thread.start()
--- examples/Qwen2.5-Coder-Instruct.py
+++ examples/Qwen2.5-Coder-Instruct.py
@@ -2,7 +2,6 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 model_name = "Qwen/Qwen2.5-Coder-0.5B-Instruct"
 
-# Now you do not need to add "trust_remote_code=True"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(
     model_name,
@@ -10,10 +9,6 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map="auto"
 )
 
-# tokenize the input into tokens
-
-# Instead of using model.chat(), we directly use model.generate()
-# But you need to use tokenizer.apply_chat_template() to format your inputs as shown below
 prompt = "write a quick sort algorithm."
 messages = [
     {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
@@ -25,11 +20,9 @@ input_ids = tokenizer.apply_chat_template(
     return_tensors="pt"
 ).to(model.device)
 
-# Directly use generate() and tokenizer.decode() to get the output.
-# Use `max_new_tokens` to control the maximum output length.
 outputs = model.generate(
     input_ids,
-    max_new_tokens=2048, # can increase the output length
+    max_new_tokens=2048,
     do_sample=True,
     temperature=0.6,
     top_p=0.9,
