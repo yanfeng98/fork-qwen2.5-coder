4783da56a8633aa4db7d5047718a649970c38215
LuYF-Lemon-love <3555028709@qq.com>
2024-11-23 21:23:51 +0800

    000

--- examples/Qwen2.5-Coder-Instruct.py
+++ examples/Qwen2.5-Coder-Instruct.py
@@ -1,10 +1,12 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-device = "cuda" # the device to load the model onto
-
 # Now you do not need to add "trust_remote_code=True"
 tokenizer = AutoTokenizer.from_pretrained("Qwen/Qwen2.5-Coder-0.5B-Instruct")
-model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen2.5-Coder-0.5B-Instruct", device_map="auto").eval()
+model = AutoModelForCausalLM.from_pretrained(
+    "Qwen/Qwen2.5-Coder-0.5B-Instruct",
+    torch_dtype="auto",
+    device_map="auto"
+)
 
 # tokenize the input into tokens
 
@@ -15,22 +17,21 @@ messages = [
     {"role": "system", "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant."},
     {"role": "user", "content": prompt}
 ]
-text = tokenizer.apply_chat_template(
+input_ids = tokenizer.apply_chat_template(
     messages,
-    tokenize=False,
-    add_generation_prompt=True
-)
-model_inputs = tokenizer([text], return_tensors="pt").to(device)
+    add_generation_prompt=True,
+    return_tensors="pt"
+).to(model.device)
 
 # Directly use generate() and tokenizer.decode() to get the output.
 # Use `max_new_tokens` to control the maximum output length.
-generated_ids = model.generate(
-    model_inputs.input_ids,
-    max_new_tokens=2048 # can increase the output length
+outputs = model.generate(
+    input_ids,
+    max_new_tokens=2048, # can increase the output length
+    do_sample=True,
+    temperature=0.6,
+    top_p=0.9,
 )
-generated_ids = [
-    output_ids[len(input_ids):] for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
-]
-response = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0]
 
-print(response)
+response = outputs[0][input_ids.shape[-1]:]
+print(tokenizer.decode(response, skip_special_tokens=True))
